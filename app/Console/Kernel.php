3a2a4095402737edf7b71f4426b0fede338f759f
Himanshu Verma <63164640+himanshu-payix@users.noreply.github.com>
2024-08-01 04:54:15 +0530

    update

--- app/Console/Kernel.php
+++ app/Console/Kernel.php
@@ -86,7 +86,8 @@ class Kernel extends ConsoleKernel
 
 
         // Now start Job Queue Worker
-        $schedule->command('queue:work --queue=order_status --timeout=60 --tries=1 --once')->name('order_status_worker')->description("Process job from order_status queue")
+        // $schedule->command('queue:work --queue=order_status --timeout=60 --tries=1 --once')->name('order_status_worker')->description("Process job from order_status queue")
+        $schedule->command('queue:work --queue=order_status')->name('order_status_worker')->description("Process job from order_status queue")
         ->everyMinute()
         ->withoutOverlapping()
         ->sendOutputTo(storage_path() . '/logs/order_status_jobs.log');
