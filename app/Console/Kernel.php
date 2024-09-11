be488418e8e1a78cf34c36397c888e84f20c061f
Himanshu Verma <63164640+himanshu-payix@users.noreply.github.com>
2024-09-11 23:48:10 +0530

    updates

--- app/Console/Kernel.php
+++ app/Console/Kernel.php
@@ -35,17 +35,22 @@ class Kernel extends ConsoleKernel
         $schedule->command('spnk:ecom_job')->name('status_update_ECOM')->description('Schedules status update job for orders in ECOM api')->everyTwoHours();
         $schedule->command('spnk:xpressbee_job')->name('status_update_XPREBEE')->description('Schedules status update job for orders in Xpressbee api')->everyTwoHours();
         
+
+        
+        /**
+         * Are already in crontab
+         */
         // Now start ECOM Job Queue Worker
-        $schedule->command('queue:work --queue=o_status_ecom --timeout=240 --tries=3 --once --backoff=3 --stop-when-empty')->name('o_status_ecom_worker')->description("Process job from order_status queue")
-        ->everyMinute()
-        // ->withoutOverlapping()
-        ->sendOutputTo(storage_path() . '/logs/order_status_jobs.log');
+        // $schedule->command('queue:work --queue=o_status_ecom --timeout=240 --tries=3 --once --backoff=3 --stop-when-empty')->name('o_status_ecom_worker')->description("Process job from order_status queue")
+        // ->everyMinute()
+        // // ->withoutOverlapping()
+        // ->sendOutputTo(storage_path() . '/logs/order_status_jobs.log');
 
         // Now start XpressBee Job Queue Worker
-        $schedule->command('queue:work --queue=o_status_xpressbee --timeout=240 --tries=3 --once --backoff=3 --stop-when-empty')->name('o_status_xpressbee_worker')->description("Process job from order_status queue")
-        ->everyMinute()
-        // ->withoutOverlapping()
-        ->sendOutputTo(storage_path() . '/logs/order_status_jobs.log');
+        // $schedule->command('queue:work --queue=o_status_xpressbee --timeout=240 --tries=3 --once --backoff=3 --stop-when-empty')->name('o_status_xpressbee_worker')->description("Process job from order_status queue")
+        // ->everyMinute()
+        // // ->withoutOverlapping()
+        // ->sendOutputTo(storage_path() . '/logs/order_status_jobs.log');
 
 
         $schedule->command('queue:retry')->hourly()->sendOutputTo(storage_path() . '/logs/job_retries.log');
