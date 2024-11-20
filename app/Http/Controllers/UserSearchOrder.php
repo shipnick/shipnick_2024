98fc080aff51b9766df94ad783e63fc1285af65a
skj2002 <108913521+skj2002@users.noreply.github.com>
2024-11-20 12:16:12 +0530

    Update UserSearchOrder.php
    merge conflict

--- app/Http/Controllers/UserSearchOrder.php
+++ app/Http/Controllers/UserSearchOrder.php
@@ -20,10 +20,6 @@ use Carbon\Carbon;
 use App\Models\smartship;
 use App\Models\orderdetail;
 use Illuminate\Support\Facades\DB;
-<<<<<<< HEAD
-=======
-
->>>>>>> 007291584fe3dce3c84c019e7301647eba3b79c1
 
 
 class UserSearchOrder extends Controller
