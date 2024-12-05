37c1363b61f88ac82501e2317a61fdc0bcf013ed
skj2002 <108913521+skj2002@users.noreply.github.com>
2024-12-05 18:51:17 +0530

    Update Dashboard.php

--- app/Http/Controllers/Dashboard.php
+++ app/Http/Controllers/Dashboard.php
@@ -161,7 +161,7 @@ public function superPanel_courier_summary()
     $rto = [ 'Shipment Redirected','Undelivered','RTO Initiated','RTO Delivered','RTO Acknowledged',         'RTO_OFD',    'RTO IN INTRANSIT','rto' ];
     $ndr = ['exception', 'Undelivered', 'RTO_NDR', 'QC FAILED'];
 
-    $awbGenBy = ['Ecom', 'Xpressbees', 'Xpressbees2', 'Bluedart'];
+    $awbGenBy = ['Ecom', 'Xpressbees' ,'Bluedart','Ekart','Bluedart-sc'];
     // $startOfMonth = Carbon::today()->startOfMonth()->startOfDay();
     // $endOfMonth = Carbon::today()->endOfMonth()->endOfDay();
     $commonConditions = [
