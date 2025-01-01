b1b9e2da1c448515044dc914d1c664f739be4f01
Himanshu Verma <himan.verma@live.com>
2025-01-01 14:39:49 +0530

    Consignment Timer changes

--- app/Console/Commands/ConsignmentStatusUpdate_CMD.php
+++ app/Console/Commands/ConsignmentStatusUpdate_CMD.php
@@ -52,33 +52,34 @@ class ConsignmentStatusUpdate_CMD extends Command
 
         foreach ($consignments as $consignment) {
             $now = Carbon::now();
+            $createAt = Carbon::parse($consignment->created_at);
             $notes = '--';
             $this->info($consignment->id . ' | Time: ' . $now);
             // After 3 pm it should show: “Parcel Picked up”
-            if ($now->hour >= 15 && $consignment->status == ConsignmentStatusUpdate::STATUS_PICKUP_SCHEDULED) {
+            if ($now->hour >= rand(4, 10) && $consignment->status == ConsignmentStatusUpdate::STATUS_PICKUP_SCHEDULED) {
                 $consignment->updateStatus(ConsignmentStatusUpdate::STATUS_PICKED_UP, $notes);
             }
 
             // After 6 from then it should show: “In-Transit”
-            if ($now->hour >= 18 && $consignment->status == ConsignmentStatusUpdate::STATUS_PICKED_UP) {
+            if ($now->gt($createAt->addHours(rand(10, 20))) && $consignment->status == ConsignmentStatusUpdate::STATUS_PICKED_UP) {
                 $consignment->updateStatus(ConsignmentStatusUpdate::STATUS_IN_TRANSIT, $notes);
             }
 
-            if ($now->hour >= 20 && $consignment->status == ConsignmentStatusUpdate::STATUS_IN_TRANSIT) {
+            if ($now->gt($createAt->addHours(rand(20, 38))) && $consignment->status == ConsignmentStatusUpdate::STATUS_IN_TRANSIT) {
                 $consignment->updateStatus(ConsignmentStatusUpdate::STATUS_SEND_TO_HUB, $notes);
             }
 
-            if ($now->hour >= 23 && $consignment->status == ConsignmentStatusUpdate::STATUS_SEND_TO_HUB) {
+            if ($now->gt($createAt->addHours(rand(38, 40))) && $consignment->status == ConsignmentStatusUpdate::STATUS_SEND_TO_HUB) {
                 $consignment->updateStatus(ConsignmentStatusUpdate::STATUS_RECEIVED_AT_HUB, $notes);
             }
 
             // After 6 hours show: “Out for delivery”
-            if ($now->diffInHours($consignment->updated_at) >= 6 && $consignment->status == ConsignmentStatusUpdate::STATUS_RECEIVED_AT_HUB) {
+            if ($now->gt($createAt->addHours(rand(40, 96))) && $consignment->status == ConsignmentStatusUpdate::STATUS_RECEIVED_AT_HUB) {
                 $consignment->updateStatus(ConsignmentStatusUpdate::STATUS_OUT_FOR_DELIVERY, $notes);
             }
 
             // After 6 hours or at 8 PM show: “Delivered”
-            if ($now->diffInHours($consignment->updated_at) > 6 && $consignment->status == ConsignmentStatusUpdate::STATUS_OUT_FOR_DELIVERY) {
+            if ($now->gt($createAt->addHours(rand(96,120))) && $consignment->status == ConsignmentStatusUpdate::STATUS_OUT_FOR_DELIVERY) {
                 $consignment->updateStatus(ConsignmentStatusUpdate::STATUS_DELIVERED, $notes);
             }
 
