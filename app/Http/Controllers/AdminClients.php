650024d4b83fb48f24d75cb9cc9c6f94f5cc6296
shashi <skj78584@gmail.com>
2025-01-23 20:09:27 +0530

    update courier

--- app/Http/Controllers/AdminClients.php
+++ app/Http/Controllers/AdminClients.php
@@ -487,33 +487,21 @@ class AdminClients extends Controller
     // Courier Assign
     public function ClientCourierPermissions(Request $req)
     {
-        $code = $req->code;
-        $courier = $req->courier;
-        $userid = $req->userid;
-        $value = $req->value;
-        $courieridno = $req->courieridno;
+        $data = [
+            'courier_code' => $req->code,
+            'courier_by' => $req->courier,
+            'user_id' => $req->userid
+        ];
 
+        $courierPermission = courierpermission::firstOrNew($data);
 
-        $courierexistsornot = courierpermission::where('courier_code', $code)
-            ->where('courier_by', $courier)
-            ->where('user_id', $userid)
-            ->get();
-        if (count($courierexistsornot)) {
-            courierpermission::where('courier_code', $code)
-                ->where('courier_by', $courier)
-                ->where('user_id', $userid)
-                ->update(['admin_flg' => $value]);
-        } else {
-            $query = new courierpermission();
-            $query->courier_idno = $courieridno;
-            $query->courier_code = $code;
-            $query->courier_by = $courier;
-            $query->user_id = $userid;
-            $query->admin_flg = $value;
-            $query->user_flg = "0";
-            $query->save();
-        }
+        $courierPermission->courier_idno = $req->courieridno;
+        $courierPermission->admin_flg = $req->value;
+        $courierPermission->user_flg = $courierPermission->exists ? $courierPermission->user_flg : "0";
+
+        $courierPermission->save();
     }
+
     // Courier Assign
 
 
